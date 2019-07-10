6287243b6f8ed22055d8f58972573277a13fce63
Joshi.Amol <amol.joshi@nihilent.com>
2019-07-10 19:31:54 +0530

    Initial Commit

---
+++ src/main/java/com/poc/custompluginclient/CustompluginclientApplication.java
@@ -0,0 +1,15 @@
+/*
+package com.poc.custompluginclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustompluginclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustompluginclientApplication.class, args);
+	}
+
+}
+*/
---
+++ src/test/java/com/poc/custompluginclient/CustompluginclientApplicationTests.java
@@ -0,0 +1,18 @@
+/*
+package com.poc.custompluginclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustompluginclientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+*/
